cf024da4268e55571b271c20256b891f8fb72a1b
Marco Fiordi <marco.fiordi97@gmail.com>
2026-03-31 16:09:18 +0200

    inserito pseudo codice e prompt

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict'
+// chiedo all'utente di inserire una parola
+
+// creo una funzione che controlla se la parola è palindroma
+
+// dentro la funzione:
+//   creo una nuova stringa vuota per la parola invertita
+
+//   ciclo la parola dalla fine all'inizio
+//   ad ogni giro aggiungo la lettera alla parola invertita
+
+//   confronto la parola originale con quella invertita
+
+//   se sono uguali:
+//       restituisco true
+//   altrimenti:
+//       restituisco false
+
+// chiamo la funzione passando la parola dell'utente
+
+// se la funzione restituisce true:
+//   stampo "la parola è palindroma"
+// altrimenti:
+//   stampo "la parola NON è palindroma"
+
+const parola = prompt('inserisci una parola');
